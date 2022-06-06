c5b811aeae25b780767a9cf0cac97285c564ce95
0xNuggan <82726722+0xNuggan@users.noreply.github.com>
2022-06-06 12:36:21 +0200

    move code to the module

--- analysis_tools/module_libraries/praise_analysis_module.py
+++ analysis_tools/module_libraries/praise_analysis_module.py
@@ -100,60 +100,6 @@ class praise_round():
         return fig
 
 
-def generate_post(params, ROOT_INPUT_PATH=""):
-    praise_path = params["system_settings"]["praise"]["input_files"]["praise_data"]
-    token_table_path = ROOT_INPUT_PATH + \
-        "distribution_results/raw_csv_exports/final_praise_token_allocation.csv"
-    params["token_allocation_per_reward_system"] = list(
-        map(int, params["token_allocation_per_reward_system"]))
-
-    data = pd.read_csv(praise_path)
-    start_date = pd.to_datetime(data["DATE"].min()).date()
-
-    end_date = pd.to_datetime(data["DATE"].max()).date()
-    total_tokens = sum(params["token_allocation_per_reward_system"])
-    praise_pct = int((
-        int(params["token_allocation_per_reward_system"][0]) / total_tokens) * 100)
-    sourcecred_pct = int((
-        int(params["token_allocation_per_reward_system"][1]) / total_tokens) * 100)
-
-    rewards_amt = int(params["system_settings"]["praise"]
-                      ["token_reward_percentages"]["contributor_rewards"]*100)
-    quant_amt = int(params["system_settings"]["praise"]
-                    ["token_reward_percentages"]["quantifier_rewards"]*100)
-    rewardboard_amt = int(params["system_settings"]["praise"]
-                          ["token_reward_percentages"]["rewardboard_rewards"]*100)
-
-    token_table = pd.read_csv(token_table_path)
-    token_table = token_table[["USER IDENTITY", "TOTAL TO RECEIVE"]].copy()
-    token_table.rename(
-        columns={'USER IDENTITY': 'Username', 'TOTAL TO RECEIVE': 'Rewards in TEC'}, inplace=True)
-    markdown_table = token_table.to_markdown(index=False)
-    output = (f'''
-# TEC Rewards Distribution - {params["distribution_name"]}  - {start_date.strftime("%d/%m/%y")} to {end_date.strftime("%d/%m/%y")}
-This period covers praise given between **{start_date.strftime("%d %B %Y")} and {end_date.strftime("%d %B %Y")}**. We allocated **{total_tokens}** TEC tokens for rewards, with a **{praise_pct}:{sourcecred_pct}** split between Praise and Sourcecred. Some praise accounts still haven’t been activated so the total amount below will be less than what we set aside to distribute.
-
-Out of the total rewards:
-
-* {rewards_amt}% of the tokens were given as praise rewards :pray:
-* {quant_amt}% distributed among quantifiers :balance_scale:
-* {rewardboard_amt}% assigned to the reward board :memo:
-
-This data has been reviewed by the Quantifiers and the Reward Board, and has been submitted for distribution to the [Reward Board DAO](https://xdai.aragon.blossom.software/#/rewardboardtec/)
-
-
-You can check out the [full period analysis here](ADD LINK HERE). :bar_chart:
-
-This post will be open to the community for review for 48 hours then submitted to the Reward Board for final execution. :heavy_check_mark:
-
-The Rewards Distribution for this round is as follows:
-''')
-
-    output += markdown_table
-
-    return output
-
-
 # courtesy of @inventandchill
 # n_senders: Left side. Praise senders. n largest ones + rest (others)
 # n_receivers: Right side. Praise receivers. n largest ones + rest (others)
@@ -203,3 +149,60 @@ def prepare_praise_flow(dataframe_in, n_senders, n_receivers):
     df4['TO'] = df4['TO']+' '
 
     return df4
+
+
+def noShow(a, b):
+    if int(a) == 0 and bool(b) == False:
+        return np.nan
+    else:
+        return a
+
+
+def spread_sort(praise_distribution, NUMBER_OF_QUANTIFIERS_PER_PRAISE):
+    # for general use
+    col_dismissed = [
+        f'DISMISSED {k+1}' for k in range(NUMBER_OF_QUANTIFIERS_PER_PRAISE)]
+    col_dupids = [
+        f'DUPLICATE ID {k+1}' for k in range(NUMBER_OF_QUANTIFIERS_PER_PRAISE)]
+    col_scores = [
+        f'SCORE {k+1}' for k in range(NUMBER_OF_QUANTIFIERS_PER_PRAISE)]
+
+    # clean the Dataframe and remove praise where there is dismissal agreement
+    praisecheck_df = praise_distribution.drop(['TO ETH ADDRESS', 'TO USER ACCOUNT ID', 'FROM ETH ADDRESS',
+                                              'FROM USER ACCOUNT ID', 'SOURCE ID', 'SOURCE NAME', 'PERCENTAGE', 'TOKEN TO RECEIVE'], axis=1)
+    ethadds = [
+        f'QUANTIFIER {k+1} ETH ADDRESS' for k in range(NUMBER_OF_QUANTIFIERS_PER_PRAISE)]
+    praisecheck_df.drop(ethadds, axis=1, inplace=True)
+
+    praisecheck_clean_controversial = praisecheck_df.loc[praisecheck_df[col_dismissed].sum(
+        axis=1) < NUMBER_OF_QUANTIFIERS_PER_PRAISE, :]
+
+    # reinstate the original scores for the duplicates before calculating the spread
+    dupclean_praisecheck = praisecheck_clean_controversial.copy()
+    for i, row in praisecheck_clean_controversial.iterrows():
+        for j, dup_id_label in enumerate(col_dupids):
+
+            if row[dup_id_label] is not None:
+                # find the score
+                find_value = praisecheck_df.loc[praisecheck_df['ID']
+                                                == row[dup_id_label], col_scores[j]]
+
+                try:
+                    # substitute it in dupclean
+                    dupclean_praisecheck.at[i, str(
+                        col_scores[j])] = int(find_value)
+                except:
+                    # account for the bug in early rounds
+                    dupclean_praisecheck.at[i, str(col_scores[j])] = 0
+
+                    # discard no-shows (score = 0 and not dismissed, after the above check for duplicates) and calculate spread
+
+    for i,  score_col in enumerate(col_scores):
+        dupclean_praisecheck[score_col] = dupclean_praisecheck.apply(
+            lambda x: noShow(x[score_col], x[col_dismissed[i]]), axis=1)
+
+    dupclean_praisecheck['SPREAD'] = dupclean_praisecheck[col_scores].max(
+        axis=1) - dupclean_praisecheck[col_scores].min(axis=1)
+    sort_by_controversial = dupclean_praisecheck.sort_values(
+        by='SPREAD', ascending=False).reset_index()
+    return sort_by_controversial
